f4e2710e1d951e00afba4a2976e4b3630028741f
Kartik2004969 <kartik3@sheridancollege.ca>
2025-01-17 16:24:33 -0500

    fetch and merge

--- src/week/pkg2/pkg1/Week21.java
+++ src/week/pkg2/pkg1/Week21.java
@@ -14,6 +14,7 @@ public class Week21 {
      * @param args the command line arguments
      */
     //showing pull
+    //fetch and merge
     public static void main(String[] args) {
         // TODO code application logic here
     }
